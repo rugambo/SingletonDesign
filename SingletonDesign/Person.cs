75cb06fb776787dec5b9a16370a1c72f130517c4
Alex Rugama <alrugama@ucsc.edu>
2024-09-09 23:35:09 -0700

    Add project files.

---
+++ SingletonDesign/Person.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+//This class is designed to show how a singleton class works and only one instance of it can exist
+namespace SingletonDesign
+{
+    internal class Person
+    {
+        static Person instance;
+        public static Person Instance {
+            get
+            {
+                if (instance != null)
+                {
+                    return instance;
+                }else{
+                    instance = new Person();
+                    return instance;
+                }
+
+            }
+            
+        }
+
+    }
+}
---
+++ SingletonDesign/Program.cs
@@ -0,0 +1,26 @@
+﻿using SingletonDesign;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        //create an instance of a Person class and see is another instance is created
+
+        Person person0 = Person.Instance;
+        Person person1 = Person.Instance;   
+
+        //Check to see if an instance has been created
+        Console.WriteLine(person0);
+        //Now call person1 is another instance or the same 
+        Console.WriteLine(person1);
+        if (person0 == person1)
+        {
+            Console.WriteLine("The same instance.");
+        }
+        else
+        {
+            Console.WriteLine("System broken");
+        }
+
+    }
+}
